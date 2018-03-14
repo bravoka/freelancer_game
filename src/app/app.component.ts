0fd6d2727de2b7614424146a5ff864aeb92b9245
Kelvin Y <kelvinyan85@gmail.com>
2018-03-14 17:29:54 +0800

    Have basic drawing/moving functionality for sprite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,46 @@ import { PlayerService } from './services/player.service';
 export class AppComponent implements AfterViewInit {
   currentRoom: Room;
 
-  @ViewChild('myRoom') room: ElementRef;
+  pos: PlayerPosition;
+
+  // private imgEle: HTMLImageElement;
+
+  @ViewChild('myRoom') room: HTMLCanvasElement;
+
+
+
+  // @ViewChild('myImg') img: ElementRef;
 
   public context: CanvasRenderingContext2D;
 
+  // public imgE: HTMLImageElement;
+
+  img: HTMLImageElement;
+
   constructor(private playerService: PlayerService) {
-  	// code...
+  	// player starting position
+
+
+  	this.pos = new PlayerPosition();
+  	// this.img.src = 'assets/bravoka.png';
+  	// this.img 'assets/bravoka.png';
+  	// this.imgEle = this.img.nativeElement;
+  	// this.imgEle.src = 'assets.bravoka.png';
+  	// this.imgE = (<HTMLImageElement>this.img.nativeElement) as HTMLImageElement;
+
+
+
+
   }
 
   ngAfterViewInit() {
-  	this.context = (<HTMLCanvasElement>this.room.nativeElement).getContext('2d');
+  	this.context = this.room['nativeElement'].getContext('2d');
+  	// this.context = this.room[0].getContext('2d');
+  	console.log(this.room);
+  	console.log(this.context);
+  	this.img = document.getElementById('myImg2') as HTMLImageElement;
+  	this.img.src = 'assets/bravoka.png';
+  	// this.context.drawImage(this.img,100,100,50,50);
   }
 
   getPlayer(): void {
@@ -29,11 +59,37 @@ export class AppComponent implements AfterViewInit {
   	this.context.fillRect(100,100,150,80);
   	this.context.fillText('hello world', 200, 200);
   }
+
+  drawImg(): void {
+  	this.context.clearRect(0, 0, this.room['nativeElement'].width, this.room['nativeElement'].height)
+  	this.context.drawImage(this.img, this.pos.X, this.pos.Y, 10, 10);
+  }
+
+  movePos(): void {
+  	this.pos.X++;
+  	this.pos.Y++;
+  	this.drawImg();
+  	
+  }
 }
 
+export class PlayerPosition {
+  X: number;
+  Y: number;
+
+  constructor() {
+  	this.X = 0;
+  	this.Y = 0;
+  }
+
+  jumpPosition(x: number, y: number) {
+  	this.X = x;
+  	this.Y = y;
+  }
+}
 
 export class Room {
-  
+
 }
 
 export class Map {
