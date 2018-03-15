cf5842e150ebcaa53e78144dc53ba5ba8d20f096
Kelvin Y <kelvinyan85@gmail.com>
2018-03-15 10:52:27 +0800

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,10 @@ import { PlayerService } from './services/player.service';
 export class AppComponent implements AfterViewInit {
   currentRoom: Room;
 
-  collisionX: boolean;
-  collisionY: boolean;
-
   collision: boolean;
-
-  // playerPos: Point;
 
   player: Rectangle;
   obstacle: Rectangle;
-
-  // playerWidth: number = 10;
-  // playerHeight: number = 10;
-
-  // obstaclePos: Point;
-
-  // obstacleWidth: number = 15;
-  // obstacleHeight: number = 15;
 
   obst: HTMLImageElement;
   // private imgEle: HTMLImageElement;
@@ -37,23 +24,18 @@ export class AppComponent implements AfterViewInit {
   @HostListener('window: keydown', ['$event'])
 
   keyEvent(event: KeyboardEvent) {
-    // console.log(event);
 
     if (event.keyCode === 39)
     {
-      // console.log('right');
       this.movePos(1, 0);
     }
     else if (event.keyCode === 37) {
-      // console.log('kleft');
       this.movePos(-1, 0);
     }
     else if (event.keyCode === 40) {
-      // console.log('down');
       this.movePos(0, 1);
     }
     else if (event.keyCode === 38) {
-      // console.log('up');
       this.movePos(0, -1);
     }
 
@@ -74,39 +56,16 @@ export class AppComponent implements AfterViewInit {
     this.player = new Rectangle(new Point(0,0), 10, 10);
 
     this.obstacle = new Rectangle(new Point(100, 100), 15, 15);
-
-  	// this.playerPos = new Point();
-    // this.obstaclePos = new Point();
-  	// this.img.src = 'assets/bravoka.png';
-  	// this.img 'assets/bravoka.png';
-  	// this.imgEle = this.img.nativeElement;
-  	// this.imgEle.src = 'assets.bravoka.png';
-  	// this.imgE = (<HTMLImageElement>this.img.nativeElement) as HTMLImageElement;
-
-
-
-
   }
 
   ngAfterViewInit() {
   	this.context = this.room['nativeElement'].getContext('2d');
-  	// this.img = this.imgRef as HTMLImageElement;
-  	// this.context = this.room[0].getContext('2d');
-  	// console.log(this.room);
-  	// console.log(this.context);
+
   	this.img = document.getElementById('myImg2') as HTMLImageElement;
   	this.img.src = 'assets/bravoka.png';
 
-    // this.obstacle.Position.X = 100;
-    // this.obstacle.Position.Y = 100;
-
     this.obst = this.renderer.createElement('img');
     this.obst.src = 'assets/obstacle.png';
-    // obst.width = 30;
-    // obst.height = 30;
-    // this.renderer.appendChild(this.room['nativeElement'], this.obst);
-
-    // this.context.drawImage(obst, 20, 20, 15, 15);
   }
 
   getPlayer(): void {
@@ -134,8 +93,6 @@ export class AppComponent implements AfterViewInit {
 
   mouseMove(event) {
     let rect = this.room['nativeElement'].getBoundingClientRect();
-    // console.log(rect);
-    // console.log(event);
 
     let x = event.clientX - rect.x;
     let y = event.clientY - rect.y;
@@ -157,12 +114,6 @@ export class AppComponent implements AfterViewInit {
   // Maybe this should take an array of objects.....? How can I attach behavior to this? Add it ot the INTERACTIVE OBJECT class perhaps??
   // TODO: Revisit this again later.
   checkForCollision(obstacle: Rectangle): boolean {
-    //Check for player's left edge
-    // if (this.player.Position.X >= obstacle.Position.X && this.player.Position.X <= obstacle.Position.X + obstacle.Width) {
-    //   if (this.player.Position.Y >= obstacle.Position.Y && this.player.Position.Y <= obstacle.Position.Y + obstacle.Height) {
-    //     return true
-    //   }
-    // }
     if (this.player.Position.X < obstacle.Position.X + obstacle.Width && this.player.Position.X + this.player.Width > obstacle.Position.X && 
       this.player.Position.Y < obstacle.Position.Y + obstacle.Height && this.player.Position.Y + this.player.Height > obstacle.Position.Y) {
       return true;
